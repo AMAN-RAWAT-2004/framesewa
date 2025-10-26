c0c800f4c2a3024086e8d58319caa7644a9545e7
AmandeepRawat <Ar464163@gmail.com>
2025-10-26 11:45:00 +0530

    Initial commit

---
+++ login.js
@@ -0,0 +1,17 @@
+
+let login=document.querySelector('#signup')
+let signup=document.querySelector('#signin')
+let logincont=document.querySelector('.login-container')
+let registercont=document.querySelector('.Signup-container')
+// let cont=document.querySelector('.container')
+login.addEventListener('click', ()=>{
+    logincont.style.display='none'
+    registercont.style.display='flex'
+    
+
+})
+signup.addEventListener('click', ()=>{
+    logincont.style.display='flex'
+    registercont.style.display='none'
+
+})
